3dafc249376d3b43d7e2a52e103b32bc77986326
Eli'ar <Use discord instead>
2026-03-18 15:28:04 +0200

    Collission added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
+import sys
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-from logger import log_state
+from logger import log_state, log_event
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
@@ -51,6 +52,12 @@ def main():
 
         milliseconds_passed = clock.tick(60)
         dt = milliseconds_passed / 1000
+
+        for asteroid in asteroids:
+             if asteroid.collides_with(ship):
+                log_event("player_hit")
+                print("Game Over!")
+                sys.exit()
 
 
 
