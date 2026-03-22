21ce45a953ce23bb68111d94e6d786ff9e4a6f3e
Eli'ar <epixai@gmail.com>
2026-03-22 12:47:09 +0200

    Splitting asteroids

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ def main():
                 if asteroid.collides_with(shot):
                     log_event("asteroid_shot")
                     shot.kill()
-                    asteroid.kill()
+                    asteroid.split()
 
 
 if __name__ == "__main__":
